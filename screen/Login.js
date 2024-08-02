eec18417fcabaf6cdc4ee7edc82828f5144a0e50
NaumanRecat <naumantariq2977@gmail.com>
2024-08-02 19:12:01 +0500

    Login Screen Done

--- screen/Login.js
+++ screen/Login.js
@@ -11,7 +11,8 @@ import Document from "./navigator/Document";
 const Login = (props) => {
     return (
         <View style={{ flex: 1, backgroundColor: BackgroundClr }}>
-            <Header  onBackPress={() => props.navigation.navigate('LandingPage')} />
+
+            <Header onBackPress={() => props.navigation.navigate('LandingPage')} />
 
             <View style={{ height: H(15), marginTop: H(5), alignSelf: 'center', marginRight: W(3) }}>
                 <Text style={{ fontSize: H(4), color: 'red', marginLeft: W(3) }} >Welcome Back</Text>
@@ -19,32 +20,31 @@ const Login = (props) => {
                 <Text style={{ fontSize: H(2.5), color: 'grey', fontWeight: 'bold' }} >CREDENTIALS TO LOGIN</Text>
             </View>
 
-            <SocialMediaIcons/>
+            <SocialMediaIcons />
 
-            <Text style={{color:'white', marginLeft:W(10),marginBottom:H(1), marginTop:H(3)}}>Email</Text>
-            <Input/>
+            <Text style={{ color: 'white', marginLeft: W(10), marginBottom: H(1), marginTop: H(3) }}>Email</Text>
+            <Input placeholdertxt="Valid email addres" />
 
-            <Text style={{color:'white', marginLeft:W(10),marginBottom:H(1), marginTop:H(2)}}>Password</Text>
-            <Input/>
+            <Text style={{ color: 'white', marginLeft: W(10), marginBottom: H(1), marginTop: H(2) }}>Password</Text>
+            <Input placeholdertxt="Use a strong password" />
 
             <View style={{
-               alignItems:'flex-end',
-               justifyContent:'flex-end',
-                width:W(90),
-                marginTop:H(2.5),
-                marginBottom:H(2.5)
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end',
+                width: W(90),
+                marginTop: H(2.5),
+                marginBottom: H(2.5)
             }}>
-            <Text style={{color:'#FFD497', }}>Forget Password</Text>
+                <Text style={{ color: '#FFD497', }}>Forget Password</Text>
             </View>
 
-            <Button onPressButton={()=>{props.navigation.navigate('BottomTabNavigator')}} alignSelf='center' txt='LOGIN' />
+            <Button onPressButton={() => { props.navigation.navigate('BottomTabNavigator') }} alignSelf='center' txt='LOGIN' />
 
-            <View style={{flexDirection:'row', marginTop:H(5), alignItems:'center', justifyContent:'center'}}>
-            <Text style={{color:'white'}}>Don't have an account?</Text>
-            <TouchableOpacity onPress={()=>props.navigation.navigate('Signup')} ><Text style={{color:'#FFD497'}}>  Sign up</Text></TouchableOpacity>
-            
+            <View style={{ flexDirection: 'row', marginTop: H(5), alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ color: 'white' }}>Don't have an account?</Text>
+                <TouchableOpacity onPress={() => props.navigation.navigate('Signup')} ><Text style={{ color: '#FFD497' }}>  Sign up</Text></TouchableOpacity>
             </View>
-            
+
 
         </View>
     )
--- screen/components/Input.js
+++ screen/components/Input.js
@@ -2,9 +2,9 @@ import React from "react";
 import {TextInput } from "react-native";
 import {H,W} from '../constant/Common';
 
-const Input = () => {
+const Input = (props) => {
     return (
-       <TextInput placeholder="Type Your Text" placeholderTextColor={'grey'} style={{height:H(6), width:W(80), borderWidth:H(0.25), borderColor:'grey', alignSelf:'center', borderRadius:H(1.5)}} />
+       <TextInput placeholder={props.placeholdertxt} placeholderTextColor={'grey'} style={{height:H(6), width:W(80), borderWidth:H(0.25), borderColor:'grey', alignSelf:'center', borderRadius:H(1.5)}} />
     )
 }
 export default Input
